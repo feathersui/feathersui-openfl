81921ffc883750a5f6e05edb929813e9fef52e0a
Josh Tynjala <joshtynjala@gmail.com>
2019-06-08 14:59:41 -0700

    Check, Radio: initial commit

---
+++ src/feathers/controls/Radio.hx
@@ -0,0 +1,24 @@
+/*
+	Feathers
+	Copyright 2019 Bowler Hat LLC. All Rights Reserved.
+
+	This program is free software. You can redistribute and/or modify it in
+	accordance with the terms of the accompanying license agreement.
+ */
+
+package feathers.controls;
+
+import feathers.style.IStyleObject;
+
+/**
+	@since 1.0.0
+**/
+class Radio extends ToggleButton {
+	public function new() {
+		super();
+	}
+
+	override private function get_styleContext():Class<IStyleObject> {
+		return Radio;
+	}
+}
