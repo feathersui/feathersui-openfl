9a050e63ed89b10fb82222d4669eeb26c7e96460
Josh Tynjala <joshtynjala@bowlerhat.dev>
2019-08-05 10:12:49 -0700

    missing header

--- src/feathers/core/IDisplayObject.hx
+++ src/feathers/core/IDisplayObject.hx
@@ -1,8 +1,14 @@
+/*
+	Feathers
+	Copyright 2019 Bowler Hat LLC. All Rights Reserved.
+
+	This program is free software. You can redistribute and/or modify it in
+	accordance with the terms of the accompanying license agreement.
+ */
+
 package feathers.core;
 
 import openfl.events.IEventDispatcher;
-import openfl.display.DisplayObject;
-import openfl.display.Stage;
 
 interface IDisplayObject extends IEventDispatcher {
 	#if flash
