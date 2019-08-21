0e6f1debf650f75e0df3f17d9ca4496139819ca2
Josh Tynjala <joshtynjala@bowlerhat.dev>
2019-08-21 15:01:53 -0700

    components-explorer: updated AssetLoaderScreen

--- examples/components-explorer/src/com/feathersui/components/screens/AssetLoaderScreen.hx
+++ examples/components-explorer/src/com/feathersui/components/screens/AssetLoaderScreen.hx
@@ -14,7 +14,8 @@ import feathers.controls.AssetLoader;
 import feathers.controls.Panel;
 
 class AssetLoaderScreen extends Panel {
-	private var assetLoader:AssetLoader;
+	private var syncAssetLoader:AssetLoader;
+	private var asyncAssetLoader:AssetLoader;
 	private var urlLoader:AssetLoader;
 
 	override private function initialize():Void {
@@ -46,13 +47,20 @@ class AssetLoaderScreen extends Panel {
 			return header;
 		};
 
-		this.assetLoader = new AssetLoader();
-		// uses openfl.Assets.getBitmapData() to get an asset by ID
-		this.assetLoader.source = "favicon";
-		this.addChild(this.assetLoader);
+		this.syncAssetLoader = new AssetLoader();
+		// uses openfl.Assets.getBitmapData() to get an embedded asset by ID
+		// <assets id="haxe" path="assets/img/haxe.png"/>
+		this.syncAssetLoader.source = "haxe";
+		this.addChild(this.syncAssetLoader);
+
+		this.asyncAssetLoader = new AssetLoader();
+		// uses openfl.Assets.loadBitmap() to get a non-embedded asset by ID
+		// <assets id="openfl" path="assets/img/openfl.png" embed="false"/>
+		this.asyncAssetLoader.source = "openfl";
+		this.addChild(this.asyncAssetLoader);
 
 		this.urlLoader = new AssetLoader();
-		// may also load an image from a URL
+		// uses openfl.display.Loader to load an image from the web
 		this.urlLoader.source = "https://feathersui.com/examples/components-explorer/images/feathersui-icon.png";
 		this.addChild(this.urlLoader);
 	}
