ce8222d6a035a76d5b63376e7d731a5ec9e669f9
Josh Tynjala <joshtynjala@bowlerhat.dev>
2019-12-06 16:16:53 -0800

    custom-theme: new example

---
+++ examples/custom-theme/src/CustomTheme.hx
@@ -0,0 +1,61 @@
+import feathers.themes.ClassVariantTheme;
+import openfl.text.TextFormat;
+import openfl.display.GradientType;
+import feathers.skins.RectangleSkin;
+import feathers.controls.Button;
+
+class CustomTheme extends ClassVariantTheme {
+	public static final VARIANT_FANCY_BUTTON:String = "custom-fancy-button";
+
+	public function new() {
+		super();
+
+		// to provide default styles, pass null for the variant
+		this.styleProvider.setStyleFunction(Button, null, setButtonStyles);
+
+		// custom themes may provide their own unique variants
+		this.styleProvider.setStyleFunction(Button, VARIANT_FANCY_BUTTON, setFancyButtonStyles);
+	}
+
+	private function setButtonStyles(button:Button):Void {
+		var backgroundSkin = new RectangleSkin();
+		backgroundSkin.border = SolidColor(1.0, 0x000000);
+		backgroundSkin.fill = SolidColor(0xcccccc);
+		button.backgroundSkin = backgroundSkin;
+
+		var downSkin = new RectangleSkin();
+		downSkin.border = SolidColor(1.0, 0x000000);
+		downSkin.fill = SolidColor(0xeeeeee);
+		button.setSkinForState(DOWN, downSkin);
+
+		var format = new TextFormat("_sans", 16, 0x232323);
+		button.textFormat = format;
+
+		button.paddingTop = 6.0;
+		button.paddingBottom = 6.0;
+		button.paddingLeft = 6.0;
+		button.paddingRight = 6.0;
+	}
+
+	private function setFancyButtonStyles(button:Button):Void {
+		var backgroundSkin = new RectangleSkin();
+		backgroundSkin.cornerRadius = 10.0;
+		backgroundSkin.border = Gradient(2, GradientType.LINEAR, [0xff9999, 0xcc0000], [1.0, 1.0], [0, 255], 90 * Math.PI / 180);
+		backgroundSkin.fill = Gradient(GradientType.LINEAR, [0xff9999, 0xff0000], [1.0, 1.0], [0, 255], 90 * Math.PI / 180);
+		button.backgroundSkin = backgroundSkin;
+
+		var downSkin = new RectangleSkin();
+		downSkin.cornerRadius = 10.0;
+		downSkin.border = Gradient(2, GradientType.LINEAR, [0xff0000, 0xcc0000], [1.0, 1.0], [0, 255], 90 * Math.PI / 180);
+		downSkin.fill = Gradient(GradientType.LINEAR, [0xff9999, 0xff0000], [1.0, 1.0], [0, 255], 270 * Math.PI / 180);
+		button.setSkinForState(DOWN, downSkin);
+
+		var format = new TextFormat("_sans", 20, 0xffeeee, true, true);
+		button.textFormat = format;
+
+		button.paddingTop = 10.0;
+		button.paddingBottom = 10.0;
+		button.paddingLeft = 20.0;
+		button.paddingRight = 20.0;
+	}
+}
---
+++ examples/custom-theme/src/Main.hx
@@ -0,0 +1,74 @@
+import feathers.controls.Panel;
+import feathers.controls.LayoutGroup;
+import feathers.controls.Label;
+import com.feathersui.controls.PoweredByFeathersUI;
+import feathers.layout.VerticalLayout;
+import feathers.style.Theme;
+import feathers.layout.AnchorLayoutData;
+import feathers.layout.AnchorLayout;
+import feathers.controls.Application;
+import feathers.controls.Button;
+
+class Main extends Application {
+	public function new() {
+		super();
+
+		Theme.setTheme(new CustomTheme());
+
+		// just some bootstrapping code for our app
+		this.initializeView();
+
+		var description = new Label();
+		description.text = "The buttons are styled by the custom theme, but everything else uses the default theme.";
+		this.view.addChild(description);
+
+		var themedButton = new Button();
+		themedButton.text = "Themed Button";
+		this.view.addChild(themedButton);
+
+		var fancyButton = new Button();
+		// a custom theme may provide custom variants
+		fancyButton.variant = CustomTheme.VARIANT_FANCY_BUTTON;
+		fancyButton.text = "Fancy Themed Button";
+		this.view.addChild(fancyButton);
+	}
+
+	private var view:Panel;
+
+	private function initializeView():Void {
+		this.layout = new AnchorLayout();
+
+		this.view = new Panel();
+		this.view.layoutData = AnchorLayoutData.fill();
+		this.view.headerFactory = () -> {
+			var header = new LayoutGroup();
+			header.variant = LayoutGroup.VARIANT_TOOL_BAR;
+			header.layout = new AnchorLayout();
+
+			var title = new Label();
+			title.variant = Label.VARIANT_HEADING;
+			title.text = "Custom Theme";
+			title.layoutData = AnchorLayoutData.center();
+			header.addChild(title);
+
+			return header;
+		};
+		this.view.footerFactory = () -> {
+			var footer = new LayoutGroup();
+
+			footer.variant = LayoutGroup.VARIANT_TOOL_BAR;
+			footer.layout = new AnchorLayout();
+			var poweredBy = new PoweredByFeathersUI();
+
+			poweredBy.layoutData = AnchorLayoutData.center();
+			footer.addChild(poweredBy);
+			return footer;
+		};
+		var viewLayout = new VerticalLayout();
+		viewLayout.horizontalAlign = CENTER;
+		viewLayout.verticalAlign = MIDDLE;
+		viewLayout.gap = 20.0;
+		this.view.layout = viewLayout;
+		this.addChild(this.view);
+	}
+}
