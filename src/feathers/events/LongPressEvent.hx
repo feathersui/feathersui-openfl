51c6d82a43071fe7a2ddf3595499d3ebdb061053
Josh Tynjala <joshtynjala@bowlerhat.dev>
2020-03-05 17:31:49 -0800

    LongPress utility and event

---
+++ src/feathers/events/LongPressEvent.hx
@@ -0,0 +1,303 @@
+/*
+	Feathers UI
+	Copyright 2020 Bowler Hat LLC. All Rights Reserved.
+
+	This program is free software. You can redistribute and/or modify it in
+	accordance with the terms of the accompanying license agreement.
+ */
+
+package feathers.events;
+
+import openfl._internal.utils.ObjectPool;
+import openfl.display.InteractiveObject;
+import openfl.errors.ArgumentError;
+import openfl.events.Event;
+import openfl.events.EventType;
+import openfl.events.IEventDispatcher;
+import openfl.events.MouseEvent;
+import openfl.events.TouchEvent;
+
+/**
+	Dispatched with a short delay after `MouseEvent.MOUSE_DOWN` or
+	TouchEvent.TOUCH_BEGIN`.
+
+	@since 1.0.0
+**/
+class LongPressEvent extends Event {
+	/**
+		The `LongPressEvent.LONG_PRESS` event type is dispatched when a button
+		is long pressed. If dispatched, the button will not dispatch
+		`TriggerEvent.TRIGGER`.
+
+		@since 1.0.0
+	**/
+	public static inline var LONG_PRESS:EventType<LongPressEvent> = "longPress";
+
+	private static var _pool = new ObjectPool<LongPressEvent>(() -> return new LongPressEvent(null), (event) -> {
+		event.__preventDefault = false;
+		event.__isCanceled = false;
+		event.__isCanceledNow = false;
+	});
+
+	/**
+		Dispatches a pooled event with the specified properties.
+
+		```hx
+		LongPressEvent.dispatchFromMouseEvent(component, event);
+		```
+
+		@since 1.0.0
+	**/
+	public static function dispatchFromMouseEvent(dispatcher:IEventDispatcher, mouseEvent:MouseEvent):Bool {
+		#if flash
+		var event = fromMouseEvent(mouseEvent);
+		return dispatcher.dispatchEvent(event);
+		#else
+		var event = _pool.get();
+		event = fromMouseEvent(mouseEvent, event);
+		var result = dispatcher.dispatchEvent(event);
+		_pool.release(event);
+		return result;
+		#end
+	}
+
+	/**
+		Dispatches a pooled event with the specified properties.
+
+		```hx
+		LongPressEvent.dispatchFromTouchEvent(component, event);
+		```
+
+		@since 1.0.0
+	**/
+	public static function dispatchFromTouchEvent(dispatcher:IEventDispatcher, touchEvent:TouchEvent):Bool {
+		#if flash
+		var event = fromTouchEvent(touchEvent);
+		return dispatcher.dispatchEvent(event);
+		#else
+		var event = _pool.get();
+		event = fromTouchEvent(touchEvent, event);
+		var result = dispatcher.dispatchEvent(event);
+		_pool.release(event);
+		return result;
+		#end
+	}
+
+	/**
+		Creates a new `LongPressEvent` object from an existing `MouseEvent`
+		object of type `MouseEvent.MOUSE_DOWN`.
+	**/
+	public static function fromMouseEvent(event:MouseEvent, ?existing:LongPressEvent):LongPressEvent {
+		if (event.type != MouseEvent.MOUSE_DOWN) {
+			throw new ArgumentError("LongPressEvent.fromMouseEvent() requires MouseEvent.MOUSE_DOWN");
+		}
+		#if !flash
+		if (existing != null) {
+			existing.type = LongPressEvent.LONG_PRESS;
+			existing.bubbles = false; // Feathers UI events don't bubble
+			existing.cancelable = event.cancelable;
+			existing.touchPointID = -1;
+			existing.isPrimaryTouchPoint = false;
+			existing.localX = event.localX;
+			existing.localY = event.localY;
+			existing.stageX = event.stageX;
+			existing.stageY = event.stageY;
+			existing.sizeX = 0.0;
+			existing.sizeY = 0.0;
+			existing.pressure = 1.0;
+			existing.relatedObject = event.relatedObject;
+			existing.ctrlKey = event.ctrlKey;
+			existing.altKey = event.altKey;
+			existing.shiftKey = event.shiftKey;
+			existing.commandKey = event.commandKey;
+			return existing;
+		}
+		#end
+		var result = new LongPressEvent(LongPressEvent.LONG_PRESS, event.bubbles, event.cancelable, -1, false, event.localX, event.localY, 0.0, 0.0, 1.0,
+			event.relatedObject, event.ctrlKey, event.altKey, event.shiftKey, Reflect.hasField(event, "commandKey") ? event.commandKey : false);
+		result.stageX = event.stageX;
+		result.stageY = event.stageY;
+		return result;
+	}
+
+	/**
+		Creates a new `LongPressEvent` object from an existing `TouchEvent`
+		object of type `TouchEvent.TOUCH_BEGIN`.
+	**/
+	public static function fromTouchEvent(event:TouchEvent, ?existing:LongPressEvent):LongPressEvent {
+		if (event.type != TouchEvent.TOUCH_BEGIN) {
+			throw new ArgumentError("LongPressEvent.fromTouchEvent() requires TouchEvent.TOUCH_BEGIN");
+		}
+		#if !flash
+		if (existing != null) {
+			existing.type = LongPressEvent.LONG_PRESS;
+			existing.bubbles = false; // Feathers UI events don't bubble
+			existing.cancelable = event.cancelable;
+			existing.touchPointID = event.touchPointID;
+			existing.isPrimaryTouchPoint = event.isPrimaryTouchPoint;
+			existing.localX = event.localX;
+			existing.localY = event.localY;
+			existing.stageX = event.stageX;
+			existing.stageY = event.stageY;
+			existing.sizeX = event.sizeX;
+			existing.sizeY = event.sizeY;
+			existing.pressure = event.pressure;
+			existing.relatedObject = event.relatedObject;
+			existing.ctrlKey = event.ctrlKey;
+			existing.altKey = event.altKey;
+			existing.shiftKey = event.shiftKey;
+			existing.commandKey = event.commandKey;
+			return existing;
+		}
+		#end
+		var result = new LongPressEvent(LongPressEvent.LONG_PRESS, event.bubbles, event.cancelable, event.touchPointID, event.isPrimaryTouchPoint,
+			event.localX, event.localY, event.sizeX, event.sizeY, event.pressure, event.relatedObject, event.ctrlKey, event.altKey, event.shiftKey,
+			event.commandKey);
+		result.stageX = event.stageX;
+		result.stageY = event.stageY;
+		return result;
+	}
+
+	private function new(type:String, bubbles:Bool = false, cancelable:Bool = false, touchPointID:Int = 0, isPrimaryTouchPoint:Bool = false,
+			localX:Float = 0.0, localY:Float = 0.0, sizeX:Float = 0.0, sizeY:Float = 0.0, pressure:Float = 1.0, ?relatedObject:InteractiveObject,
+			ctrlKey:Bool = false, altKey:Bool = false, shiftKey:Bool = false, commandKey:Bool = false) {
+		super(type, bubbles, cancelable);
+		this.touchPointID = touchPointID;
+		this.isPrimaryTouchPoint = isPrimaryTouchPoint;
+		this.localX = localX;
+		this.localY = localY;
+		this.stageX = Math.NaN;
+		this.stageY = Math.NaN;
+		this.sizeX = sizeX;
+		this.sizeY = sizeY;
+		this.pressure = pressure;
+		this.relatedObject = relatedObject;
+		this.ctrlKey = ctrlKey;
+		this.altKey = altKey;
+		this.shiftKey = shiftKey;
+		this.commandKey = commandKey;
+	}
+
+	/**
+		@see `openfl.events.TouchEvent.touchPointID`
+
+		@since 1.0.0
+	**/
+	public var touchPointID:Int;
+
+	/**
+		@see `openfl.events.TouchEvent.isPrimaryTouchPoint`
+
+		@since 1.0.0
+	**/
+	public var isPrimaryTouchPoint:Bool;
+
+	/**
+		@see `openfl.events.MouseEvent.altKey`
+		@see `openfl.events.TouchEvent.altKey`
+
+		@since 1.0.0
+	**/
+	public var altKey:Bool;
+
+	/**
+		@see `openfl.events.MouseEvent.commandKey`
+		@see `openfl.events.TouchEvent.commandKey`
+
+		@since 1.0.0
+	**/
+	public var commandKey:Bool;
+
+	/**
+		@see `openfl.events.MouseEvent.ctrlKey`
+		@see `openfl.events.TouchEvent.ctrlKey`
+
+		@since 1.0.0
+	**/
+	public var ctrlKey:Bool;
+
+	/**
+		@see `openfl.events.MouseEvent.controlKey`
+		@see `openfl.events.TouchEvent.controlKey`
+
+		@since 1.0.0
+	**/
+	public var controlKey:Bool;
+
+	/**
+		@see `openfl.events.MouseEvent.shiftKey`
+		@see `openfl.events.TouchEvent.shiftKey`
+
+		@since 1.0.0
+	**/
+	public var shiftKey:Bool;
+
+	/**
+		@see `openfl.events.MouseEvent.localX`
+		@see `openfl.events.TouchEvent.localX`
+
+		@since 1.0.0
+	**/
+	public var localX:Float;
+
+	/**
+		@see `openfl.events.MouseEvent.localY`
+		@see `openfl.events.TouchEvent.localY`
+
+		@since 1.0.0
+	**/
+	public var localY:Float;
+
+	/**
+		@see `openfl.events.TouchEvent.sizeX`
+
+		@since 1.0.0
+	**/
+	public var sizeX:Float;
+
+	/**
+		@see `openfl.events.TouchEvent.sizeY`
+
+		@since 1.0.0
+	**/
+	public var sizeY:Float;
+
+	/**
+		@see `openfl.events.TouchEvent.pressure`
+
+		@since 1.0.0
+	**/
+	public var pressure:Float;
+
+	/**
+		@see `openfl.events.MouseEvent.stageX`
+		@see `openfl.events.TouchEvent.stageX`
+
+		@since 1.0.0
+	**/
+	public var stageX:Float;
+
+	/**
+		@see `openfl.events.MouseEvent.stageY`
+		@see `openfl.events.TouchEvent.stageY`
+
+		@since 1.0.0
+	**/
+	public var stageY:Float;
+
+	/**
+		@see `openfl.events.MouseEvent.relatedObject`
+		@see `openfl.events.TouchEvent.relatedObject`
+
+		@since 1.0.0
+	**/
+	public var relatedObject:InteractiveObject;
+
+	override public function clone():Event {
+		var result = new LongPressEvent(this.type, this.bubbles, this.cancelable, this.touchPointID, this.isPrimaryTouchPoint, this.localX, this.localY,
+			this.sizeX, this.sizeY, this.pressure, this.relatedObject, this.ctrlKey, this.altKey, this.shiftKey, this.commandKey);
+		result.stageX = this.stageX;
+		result.stageY = this.stageY;
+		return result;
+	}
+}
---
+++ src/feathers/utils/LongPress.hx
@@ -0,0 +1,226 @@
+/*
+	Feathers UI
+	Copyright 2020 Bowler Hat LLC. All Rights Reserved.
+
+	This program is free software. You can redistribute and/or modify it in
+	accordance with the terms of the accompanying license agreement.
+ */
+
+package feathers.utils;
+
+import feathers.events.TriggerEvent;
+import openfl.Lib;
+import feathers.events.LongPressEvent;
+import openfl.display.Stage;
+import openfl.display.InteractiveObject;
+import openfl.events.Event;
+import openfl.events.MouseEvent;
+import openfl.events.TouchEvent;
+#if air
+import openfl.ui.Multitouch;
+#end
+
+/**
+	Dispatches `LongPressEvent.LONG_PRESS` (or a custom event type) on mouse
+	down or touch begin after a short delay.
+
+	@see `feathers.controls.Button`
+	@see `feathers.events.LongPressEvent.LONG_PRESS`
+
+	@since 1.0.0
+**/
+class LongPress {
+	/**
+		Creates a new `LongPress` object with the given arguments.
+
+		@since 1.0.0
+	**/
+	public function new(target:InteractiveObject = null, ?eventFactory:() -> Event) {
+		this.target = target;
+		this.eventFactory = eventFactory;
+	}
+
+	/**
+		The target component that should dispatch the event.
+
+		@since 1.0.0
+	**/
+	public var target(default, set):InteractiveObject = null;
+
+	private function set_target(value:InteractiveObject):InteractiveObject {
+		if (this.target == value) {
+			return this.target;
+		}
+		if (this.target != null) {
+			this.cleanupMouseEvents();
+			this.cleanupTouchEvents();
+			this.target.removeEventListener(MouseEvent.MOUSE_DOWN, target_mouseDownHandler);
+			this.target.removeEventListener(TouchEvent.TOUCH_BEGIN, target_touchBeginHandler);
+			this.target.removeEventListener(TriggerEvent.TRIGGER, target_triggerHandler);
+		}
+		this.target = value;
+		if (this.target != null) {
+			this.target.addEventListener(MouseEvent.MOUSE_DOWN, target_mouseDownHandler);
+			this.target.addEventListener(TouchEvent.TOUCH_BEGIN, target_touchBeginHandler);
+			this.target.addEventListener(TriggerEvent.TRIGGER, target_triggerHandler, false, 10);
+		}
+		return this.target;
+	}
+
+	/**
+		The duration, measured in seconds, of a long press.
+
+		The following example changes the long press duration to one second:
+
+		```hx
+		longPress.duration = 1.0;
+		```
+
+		@since 1.0.0
+	**/
+	public var duration:Float = 0.5;
+
+	/**
+		The event type to dispatch on long press.
+
+		@since 1.0.0
+	**/
+	public var eventFactory(default, set):() -> Event = null;
+
+	private function set_eventFactory(value:() -> Event):() -> Event {
+		if (this.eventFactory == value) {
+			return this.eventFactory;
+		}
+		this.eventFactory = value;
+		return eventFactory;
+	}
+
+	/**
+		May be set to `false` to disable the long press event temporarily until
+		set back to `true`.
+
+		@default true
+
+		@since 1.0.0
+	**/
+	public var enabled(default, default):Bool = true;
+
+	private var _savedMouseEvent:MouseEvent;
+	private var _savedTouchEvent:TouchEvent;
+	private var _startTime:Float;
+	private var _stopNextTrigger:Bool = false;
+
+	private function target_mouseDownHandler(event:MouseEvent):Void {
+		if (!this.enabled) {
+			return;
+		}
+		if (this.eventFactory != null) {
+			this.target.dispatchEvent(this.eventFactory());
+			return;
+		}
+		this._startTime = Lib.getTimer();
+		this._stopNextTrigger = false;
+		this._savedMouseEvent = event.clone();
+		var stage:Stage = this.target.stage;
+		stage.addEventListener(MouseEvent.MOUSE_MOVE, target_stage_mouseMoveHandler, false, 0, true);
+		stage.addEventListener(MouseEvent.MOUSE_UP, target_stage_mouseUpHandler, false, 0, true);
+		this.target.addEventListener(Event.ENTER_FRAME, target_enterFrameHandler);
+	}
+
+	private function target_touchBeginHandler(event:TouchEvent):Void {
+		if (!this.enabled) {
+			return;
+		}
+		if (event.isPrimaryTouchPoint #if air && Multitouch.mapTouchToMouse #end) {
+			// ignore the primary one because MouseEvent.MOUSE_DOWN will catch it
+			return;
+		}
+		if (this.eventFactory != null) {
+			this.target.dispatchEvent(this.eventFactory());
+			return;
+		}
+		this._startTime = Lib.getTimer();
+		this._stopNextTrigger = false;
+		this._savedTouchEvent = event.clone();
+		var stage:Stage = this.target.stage;
+		stage.addEventListener(TouchEvent.TOUCH_MOVE, target_stage_touchMoveHandler, false, 0, true);
+		stage.addEventListener(TouchEvent.TOUCH_END, target_stage_touchEndHandler, false, 0, true);
+		this.target.addEventListener(Event.ENTER_FRAME, target_enterFrameHandler);
+	}
+
+	private function target_stage_mouseMoveHandler(event:MouseEvent):Void {
+		this._savedMouseEvent.localX = event.localX;
+		this._savedMouseEvent.localY = event.localY;
+		this._savedMouseEvent.stageX = event.stageX;
+		this._savedMouseEvent.stageY = event.stageY;
+		this._savedMouseEvent.altKey = event.altKey;
+		this._savedMouseEvent.ctrlKey = event.ctrlKey;
+		this._savedMouseEvent.commandKey = event.commandKey;
+		this._savedMouseEvent.shiftKey = event.shiftKey;
+	}
+
+	private function target_stage_touchMoveHandler(event:TouchEvent):Void {
+		if (this._savedTouchEvent.touchPointID != event.touchPointID) {
+			return;
+		}
+		this._savedTouchEvent.localX = event.localX;
+		this._savedTouchEvent.localY = event.localY;
+		this._savedTouchEvent.stageX = event.stageX;
+		this._savedTouchEvent.stageY = event.stageY;
+		this._savedMouseEvent.altKey = event.altKey;
+		this._savedMouseEvent.ctrlKey = event.ctrlKey;
+		this._savedMouseEvent.commandKey = event.commandKey;
+		this._savedMouseEvent.shiftKey = event.shiftKey;
+	}
+
+	private function target_stage_mouseUpHandler(event:MouseEvent):Void {
+		this.cleanupMouseEvents();
+	}
+
+	private function target_stage_touchEndHandler(event:TouchEvent):Void {
+		if (this._savedTouchEvent.touchPointID != event.touchPointID) {
+			return;
+		}
+		this.cleanupTouchEvents();
+	}
+
+	private function target_triggerHandler(event:TriggerEvent):Void {
+		if (!this._stopNextTrigger) {
+			return;
+		}
+		this._stopNextTrigger = false;
+		event.stopImmediatePropagation();
+		trace("stop propagation");
+	}
+
+	private function cleanupMouseEvents():Void {
+		var stage:Stage = this.target.stage;
+		stage.removeEventListener(MouseEvent.MOUSE_MOVE, target_stage_mouseMoveHandler);
+		stage.removeEventListener(MouseEvent.MOUSE_UP, target_stage_mouseUpHandler);
+		this.target.removeEventListener(Event.ENTER_FRAME, target_enterFrameHandler);
+		this._savedMouseEvent = null;
+	}
+
+	private function cleanupTouchEvents():Void {
+		var stage:Stage = this.target.stage;
+		stage.removeEventListener(TouchEvent.TOUCH_MOVE, target_stage_touchMoveHandler);
+		stage.removeEventListener(TouchEvent.TOUCH_END, target_stage_touchEndHandler);
+		this.target.removeEventListener(Event.ENTER_FRAME, target_enterFrameHandler);
+		this._savedTouchEvent = null;
+	}
+
+	private function target_enterFrameHandler(event:Event):Void {
+		var accumulatedTime = (Lib.getTimer() - this._startTime) / 1000.0;
+		if (accumulatedTime < this.duration) {
+			return;
+		}
+		this._stopNextTrigger = true;
+		if (this._savedMouseEvent != null) {
+			LongPressEvent.dispatchFromMouseEvent(this.target, this._savedMouseEvent);
+			this.cleanupMouseEvents();
+			return;
+		}
+		LongPressEvent.dispatchFromTouchEvent(this.target, this._savedTouchEvent);
+		this.cleanupTouchEvents();
+	}
+}
