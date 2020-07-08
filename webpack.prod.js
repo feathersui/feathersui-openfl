582159ce1ec44e81b5e3a6fe2320b44f532ea0c0
Josh Tynjala <joshtynjala@bowlerhat.dev>
2020-07-08 11:32:42 -0700

    npm: fix build that broke from updated webpack-merge plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
 const webpack = require("webpack");
-const merge = require("webpack-merge");
+const { merge } = require("webpack-merge");
 const BrotliPlugin = require("brotli-webpack-plugin");
 const CompressionPlugin = require("compression-webpack-plugin");
 const common = require("./webpack.common.js");
