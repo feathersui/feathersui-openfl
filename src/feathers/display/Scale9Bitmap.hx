47bb74fe75538af9389d1151c37e41d6807b15af
Josh Tynjala <joshtynjala@bowlerhat.dev>
2020-08-17 15:51:40 -0700

    Scale9Bitmap: draws multiple bitmaps instead of graphics.beginBitmapFill() to avoid gaps betwen sections

--- src/feathers/display/Scale9Bitmap.hx
+++ src/feathers/display/Scale9Bitmap.hx
@@ -8,11 +8,12 @@
 
 package feathers.display;
 
+import openfl.display.Bitmap;
 import openfl.geom.Matrix;
 import openfl.geom.Point;
 import openfl.errors.ArgumentError;
 import openfl.geom.Rectangle;
-import openfl.display.Shape;
+import openfl.display.Sprite;
 import openfl.display.BitmapData;
 
 /**
@@ -26,7 +27,7 @@ import openfl.display.BitmapData;
 
 	@since 1.0.0
 **/
-class Scale9Bitmap extends Shape {
+class Scale9Bitmap extends Sprite {
 	/**
 		Creates a new `Scale9Bitmap` object with the given arguments.
 
@@ -56,6 +57,16 @@ class Scale9Bitmap extends Shape {
 	private var bottomLeft:BitmapData;
 	private var bottomCenter:BitmapData;
 	private var bottomRight:BitmapData;
+
+	private var _topLeftBitmap:Bitmap;
+	private var _topCenterBitmap:Bitmap;
+	private var _topRightBitmap:Bitmap;
+	private var _middleLeftBitmap:Bitmap;
+	private var _middleCenterBitmap:Bitmap;
+	private var _middleRightBitmap:Bitmap;
+	private var _bottomLeftBitmap:Bitmap;
+	private var _bottomCenterBitmap:Bitmap;
+	private var _bottomRightBitmap:Bitmap;
 
 	private var top:Int = 0;
 	private var right:Int = 0;
@@ -186,6 +197,32 @@ class Scale9Bitmap extends Shape {
 		#end
 	}
 
+	private var _useGraphics:Bool = false;
+
+	/**
+		Controls whether or not the bitmap is drawn with
+		`openfl.display.Graphics` or with separate `Bitmap` display objects.
+
+		@since 1.0.0
+	**/
+	@:flash.property
+	public var useGraphics(get, set):Bool;
+
+	private function get_useGraphics():Bool {
+		return this._useGraphics;
+	}
+
+	private function set_useGraphics(value:Bool):Bool {
+		if (this._useGraphics == value) {
+			return this._useGraphics;
+		}
+		this._useGraphics = value;
+		if (this._allowRefresh) {
+			this.draw();
+		}
+		return this._useGraphics;
+	}
+
 	private var _matrix = new Matrix();
 	private var _point = new Point();
 	private var _rectangle = new Rectangle();
@@ -275,95 +312,176 @@ class Scale9Bitmap extends Shape {
 		this.graphics.clear();
 		if (this._width == 0 || this._height == 0) {
 			// nothing to draw
+			this.cleanupBitmaps();
 			return;
 		}
 
-		if (this._scale9Grid == null) {
-			this._matrix.identity();
-			this._matrix.scale(this._width / this._bitmapData.width, this._height / this._bitmapData.height);
-			this.graphics.beginBitmapFill(this._bitmapData, this._matrix, false, this._smoothing);
-			this.graphics.drawRect(0.0, 0.0, this._width, this._height);
-			this.graphics.endFill();
-			return;
-		}
-
-		var center2 = Math.max(0.0, this._width - left - right);
-		var middle2 = Math.max(0.0, this._height - top - bottom);
-
-		//--- top row
+		var center2 = Math.max(0.0, this._width - this.left - this.right);
+		var middle2 = Math.max(0.0, this._height - this.top - this.bottom);
 
 		if (this.topLeft != null) {
-			this._matrix.identity();
-			this.graphics.beginBitmapFill(this.topLeft, null, false, this._smoothing);
-			this.graphics.drawRect(0.0, 0.0, left, top);
-			this.graphics.endFill();
+			if (this._topLeftBitmap == null) {
+				this._topLeftBitmap = new Bitmap(null, NEVER, this._smoothing);
+				this.addChild(this._topLeftBitmap);
+			}
+			this._topLeftBitmap.bitmapData = this.topLeft;
+			this._topLeftBitmap.smoothing = this._smoothing;
+			this._topLeftBitmap.x = 0.0;
+			this._topLeftBitmap.y = 0.0;
+		} else if (this._topLeftBitmap != null) {
+			this.removeChild(this._topLeftBitmap);
+			this._topLeftBitmap = null;
 		}
 		if (this.topCenter != null) {
-			this._matrix.identity();
-			this._matrix.scale(center2 / center, 1.0);
-			this._matrix.translate(left, 0.0);
-			this.graphics.beginBitmapFill(this.topCenter, this._matrix, false, this._smoothing);
-			this.graphics.drawRect(left, 0.0, center2, top);
-			this.graphics.endFill();
+			if (this._topCenterBitmap == null) {
+				this._topCenterBitmap = new Bitmap(null, NEVER, this._smoothing);
+				this.addChild(this._topCenterBitmap);
+			}
+			this._topCenterBitmap.bitmapData = this.topCenter;
+			this._topCenterBitmap.smoothing = this._smoothing;
+			this._topCenterBitmap.x = this.left;
+			this._topCenterBitmap.y = 0.0;
+			this._topCenterBitmap.width = center2;
+		} else if (this._topCenterBitmap != null) {
+			this.removeChild(this._topCenterBitmap);
+			this._topCenterBitmap = null;
 		}
 		if (this.topRight != null) {
-			this._matrix.identity();
-			this._matrix.translate(this._width - right, 0.0);
-			this.graphics.beginBitmapFill(this.topRight, this._matrix, false, this._smoothing);
-			this.graphics.drawRect(this._width - right, 0.0, right, top);
-			this.graphics.endFill();
+			if (this._topRightBitmap == null) {
+				this._topRightBitmap = new Bitmap(null, NEVER, this._smoothing);
+				this.addChild(this._topRightBitmap);
+			}
+			this._topRightBitmap.bitmapData = this.topRight;
+			this._topRightBitmap.smoothing = this._smoothing;
+			this._topRightBitmap.x = this.left + center2;
+			this._topRightBitmap.y = 0.0;
+		} else if (this._topRightBitmap != null) {
+			this.removeChild(this._topRightBitmap);
+			this._topRightBitmap = null;
 		}
-
-		//--- middle row
 
 		if (this.middleLeft != null) {
-			this._matrix.identity();
-			this._matrix.scale(1.0, middle2 / middle);
-			this._matrix.translate(0.0, top);
-			this.graphics.beginBitmapFill(this.middleLeft, this._matrix, false, this._smoothing);
-			this.graphics.drawRect(0.0, top, left, middle2);
-			this.graphics.endFill();
+			if (this._middleLeftBitmap == null) {
+				this._middleLeftBitmap = new Bitmap(null, NEVER, this._smoothing);
+				this.addChild(this._middleLeftBitmap);
+			}
+			this._middleLeftBitmap.bitmapData = this.middleLeft;
+			this._middleLeftBitmap.smoothing = this._smoothing;
+			this._middleLeftBitmap.x = 0.0;
+			this._middleLeftBitmap.y = this.top;
+			this._middleLeftBitmap.height = middle2;
+		} else if (this._middleLeftBitmap != null) {
+			this.removeChild(this._middleLeftBitmap);
+			this._middleLeftBitmap = null;
 		}
 		if (this.middleCenter != null) {
-			this._matrix.identity();
-			this._matrix.scale(center2 / center, middle2 / middle);
-			this._matrix.translate(left, top);
-			this.graphics.beginBitmapFill(this.middleCenter, this._matrix, false, this._smoothing);
-			this.graphics.drawRect(left, top, center2, middle2);
-			this.graphics.endFill();
+			if (this._middleCenterBitmap == null) {
+				this._middleCenterBitmap = new Bitmap(null, NEVER, this._smoothing);
+				this.addChild(this._middleCenterBitmap);
+			}
+			this._middleCenterBitmap.bitmapData = this.middleCenter;
+			this._middleCenterBitmap.smoothing = this._smoothing;
+			this._middleCenterBitmap.x = this.left;
+			this._middleCenterBitmap.y = this.top;
+			this._middleCenterBitmap.width = center2;
+			this._middleCenterBitmap.height = middle2;
+		} else if (this._middleCenterBitmap != null) {
+			this.removeChild(this._middleCenterBitmap);
+			this._middleCenterBitmap = null;
 		}
 		if (this.middleRight != null) {
-			this._matrix.identity();
-			this._matrix.scale(1.0, middle2 / middle);
-			this._matrix.translate(this._width - right, top);
-			this.graphics.beginBitmapFill(this.middleRight, this._matrix, false, this._smoothing);
-			this.graphics.drawRect(this._width - right, top, right, middle2);
-			this.graphics.endFill();
+			if (this._middleRightBitmap == null) {
+				this._middleRightBitmap = new Bitmap(null, NEVER, this._smoothing);
+				this.addChild(this._middleRightBitmap);
+			}
+			this._middleRightBitmap.bitmapData = this.middleRight;
+			this._middleRightBitmap.smoothing = this._smoothing;
+			this._middleRightBitmap.x = this.left + center2;
+			this._middleRightBitmap.y = this.top;
+			this._middleRightBitmap.height = middle2;
+		} else if (this._middleRightBitmap != null) {
+			this.removeChild(this._middleRightBitmap);
+			this._middleRightBitmap = null;
 		}
-
-		//--- bottom row
 
 		if (this.bottomLeft != null) {
-			this._matrix.identity();
-			this._matrix.translate(0.0, this._height - bottom);
-			this.graphics.beginBitmapFill(this.bottomLeft, this._matrix, false, this._smoothing);
-			this.graphics.drawRect(0.0, this._height - bottom, left, bottom);
-			this.graphics.endFill();
+			if (this._bottomLeftBitmap == null) {
+				this._bottomLeftBitmap = new Bitmap(null, NEVER, this._smoothing);
+				this.addChild(this._bottomLeftBitmap);
+			}
+			this._bottomLeftBitmap.bitmapData = this.bottomLeft;
+			this._bottomLeftBitmap.smoothing = this._smoothing;
+			this._bottomLeftBitmap.x = 0.0;
+			this._bottomLeftBitmap.y = this.top + middle2;
+		} else if (this._bottomLeftBitmap != null) {
+			this.removeChild(this._bottomLeftBitmap);
+			this._bottomLeftBitmap = null;
 		}
 		if (this.bottomCenter != null) {
-			this._matrix.identity();
-			this._matrix.scale(center2 / center, 1.0);
-			this._matrix.translate(left, this._height - bottom);
-			this.graphics.beginBitmapFill(this.bottomCenter, this._matrix, false, this._smoothing);
-			this.graphics.drawRect(left, this._height - bottom, center2, bottom);
-			this.graphics.endFill();
+			if (this._bottomCenterBitmap == null) {
+				this._bottomCenterBitmap = new Bitmap(null, NEVER, this._smoothing);
+				this.addChild(this._bottomCenterBitmap);
+			}
+			this._bottomCenterBitmap.bitmapData = this.bottomCenter;
+			this._bottomCenterBitmap.smoothing = this._smoothing;
+			this._bottomCenterBitmap.x = this.left;
+			this._bottomCenterBitmap.y = this.top + middle2;
+			this._bottomCenterBitmap.width = center2;
+		} else if (this._bottomCenterBitmap != null) {
+			this.removeChild(this._bottomCenterBitmap);
+			this._bottomCenterBitmap = null;
 		}
 		if (this.bottomRight != null) {
-			this._matrix.identity();
-			this._matrix.translate(this._width - right, this._height - bottom);
-			this.graphics.beginBitmapFill(this.bottomRight, this._matrix, false, this._smoothing);
-			this.graphics.drawRect(this._width - right, this._height - bottom, right, bottom);
-			this.graphics.endFill();
+			if (this._bottomRightBitmap == null) {
+				this._bottomRightBitmap = new Bitmap(null, NEVER, this._smoothing);
+				this.addChild(this._bottomRightBitmap);
+			}
+			this._bottomRightBitmap.bitmapData = this.bottomRight;
+			this._bottomRightBitmap.smoothing = this._smoothing;
+			this._bottomRightBitmap.x = this.left + center2;
+			this._bottomRightBitmap.y = this.top + middle2;
+		} else if (this._bottomRightBitmap != null) {
+			this.removeChild(this._bottomRightBitmap);
+			this._bottomRightBitmap = null;
+		}
+	}
+
+	private function cleanupBitmaps():Void {
+		if (this._topLeftBitmap != null) {
+			this.removeChild(this._topLeftBitmap);
+			this._topLeftBitmap = null;
+		}
+		if (this._topCenterBitmap != null) {
+			this.removeChild(this._topCenterBitmap);
+			this._topCenterBitmap = null;
+		}
+		if (this._topRightBitmap != null) {
+			this.removeChild(this._topRightBitmap);
+			this._topRightBitmap = null;
+		}
+		if (this._middleLeftBitmap != null) {
+			this.removeChild(this._middleLeftBitmap);
+			this._middleLeftBitmap = null;
+		}
+		if (this._middleCenterBitmap != null) {
+			this.removeChild(this._middleCenterBitmap);
+			this._middleCenterBitmap = null;
+		}
+		if (this._middleRightBitmap != null) {
+			this.removeChild(this._middleRightBitmap);
+			this._middleRightBitmap = null;
+		}
+		if (this._bottomLeftBitmap != null) {
+			this.removeChild(this._bottomLeftBitmap);
+			this._bottomLeftBitmap = null;
+		}
+		if (this._bottomCenterBitmap != null) {
+			this.removeChild(this._bottomCenterBitmap);
+			this._bottomCenterBitmap = null;
+		}
+		if (this._bottomRightBitmap != null) {
+			this.removeChild(this._bottomRightBitmap);
+			this._bottomRightBitmap = null;
 		}
 	}
 }
