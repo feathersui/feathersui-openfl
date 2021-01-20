8c9b7712ac4c36cb8ee8d1c4bf737f8c8f5aaf4d
Josh Tynjala <joshtynjala@bowlerhat.dev>
2021-01-20 15:30:53 -0800

    Radio: keyboard arrows change selection

--- src/feathers/controls/Radio.hx
+++ src/feathers/controls/Radio.hx
@@ -8,10 +8,15 @@
 
 package feathers.controls;
 
-import feathers.themes.steel.components.SteelRadioStyles;
-import openfl.events.Event;
-import openfl.errors.IllegalOperationError;
+import feathers.core.IFocusObject;
 import feathers.core.ToggleGroup;
+import feathers.themes.steel.components.SteelRadioStyles;
+import openfl.display.InteractiveObject;
+import openfl.errors.IllegalOperationError;
+import openfl.events.Event;
+import openfl.events.FocusEvent;
+import openfl.events.KeyboardEvent;
+import openfl.ui.Keyboard;
 
 /**
 	A selectable control that may be toggled on and off and exists in a group
@@ -68,6 +73,8 @@ class Radio extends ToggleButton implements IGroupedToggle {
 		super();
 		super.toggleable = true;
 		this.addEventListener(Event.ADDED_TO_STAGE, radio_addedToStageHandler);
+		this.addEventListener(KeyboardEvent.KEY_DOWN, radio_keyDownHandler);
+		this.addEventListener(FocusEvent.FOCUS_IN, radio_focusInHandler);
 	}
 
 	override private function set_toggleable(value:Bool):Bool {
@@ -118,6 +125,63 @@ class Radio extends ToggleButton implements IGroupedToggle {
 		SteelRadioStyles.initialize();
 	}
 
+	private function navigateWithKeyboard(event:KeyboardEvent):Void {
+		if (event.isDefaultPrevented()) {
+			return;
+		}
+		if (this._toggleGroup == null) {
+			return;
+		}
+		var startIndex = this._toggleGroup.getItemIndex(this);
+		if (startIndex == -1) {
+			return;
+		}
+		var result = startIndex;
+		var needsAnotherPass = true;
+		var lastResult = -1;
+		while (needsAnotherPass) {
+			needsAnotherPass = false;
+			switch (event.keyCode) {
+				case Keyboard.UP:
+					result = result - 1;
+				case Keyboard.DOWN:
+					result = result + 1;
+				case Keyboard.LEFT:
+					result = result - 1;
+				case Keyboard.RIGHT:
+					result = result + 1;
+				default:
+					// not keyboard navigation
+					return;
+			}
+			if (result < 0) {
+				result = 0;
+			} else if (result >= this._toggleGroup.numItems) {
+				result = this._toggleGroup.numItems - 1;
+			}
+			var nextItem = this._toggleGroup.getItemAt(result);
+			if (!nextItem.enabled) {
+				// keep going until we reach a non-branch
+				if (result == lastResult) {
+					// but don't keep trying if we got the same result more than
+					// once because it means that we got stuck
+					return;
+				}
+				needsAnotherPass = true;
+			}
+			lastResult = result;
+		}
+		event.preventDefault();
+		this._toggleGroup.selectedIndex = result;
+		var nextFocus = cast(this._toggleGroup.selectedItem, IFocusObject);
+		if (this._focusManager != null) {
+			this._focusManager.focus = nextFocus;
+			nextFocus.showFocus(true);
+		} else {
+			this.stage.focus = cast(nextFocus, InteractiveObject);
+		}
+	}
+
 	private function radio_addedToStageHandler(event:Event):Void {
 		if (this._toggleGroup == null) {
 			// use the setter
@@ -130,6 +194,28 @@ class Radio extends ToggleButton implements IGroupedToggle {
 		this.removeEventListener(Event.REMOVED_FROM_STAGE, radio_removedFromStageHandler);
 		if (this._toggleGroup == defaultRadioGroup) {
 			this._toggleGroup.removeItem(this);
+		}
+	}
+
+	private function radio_keyDownHandler(event:KeyboardEvent):Void {
+		if (!this._enabled) {
+			return;
+		}
+		this.navigateWithKeyboard(event);
+	}
+
+	private function radio_focusInHandler(event:FocusEvent):Void {
+		if (this._selected) {
+			return;
+		}
+		var nextFocus = cast(this._toggleGroup.selectedItem, IFocusObject);
+		if (nextFocus == null || nextFocus == this) {
+			return;
+		}
+		if (this._focusManager != null) {
+			this._focusManager.focus = nextFocus;
+		} else {
+			this.stage.focus = cast(nextFocus, InteractiveObject);
 		}
 	}
 }
