6e5b6236ed2574aef88b46f014602ffb40ce4065
Josh Tynjala <joshtynjala@bowlerhat.dev>
2021-11-19 14:27:07 -0800

    IFocusExclusions: allow certain children to be optionally excluded from receiving focus

---
+++ src/feathers/core/IFocusExclusions.hx
@@ -0,0 +1,27 @@
+/*
+	Feathers UI
+	Copyright 2021 Bowler Hat LLC. All Rights Reserved.
+
+	This program is free software. You can redistribute and/or modify it in
+	accordance with the terms of the accompanying license agreement.
+ */
+
+package feathers.core;
+
+import openfl.display.DisplayObject;
+
+/**
+	A container that may exclude some children from receiving focus.
+
+	@since 1.0.0
+**/
+interface IFocusExclusions extends IFocusManagerAware {
+	/**
+		Display object children that should be skipped by the focus manager.
+
+		May return `null` if there are no excluded children.
+
+		@since 1.0.0
+	**/
+	var focusExclusions(get, never):Array<DisplayObject>;
+}
