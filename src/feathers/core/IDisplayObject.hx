6c2f14048d2b5be16e6c9e7048c3dbae804b487d
Josh Tynjala <joshtynjala@bowlerhat.dev>
2022-01-13 14:14:40 -0800

    IDisplayObject: see OpenFL docs

--- src/feathers/core/IDisplayObject.hx
+++ src/feathers/core/IDisplayObject.hx
@@ -22,55 +22,85 @@ import openfl.events.IEventDispatcher;
 **/
 @:dox(hide)
 interface IDisplayObject extends IEventDispatcher {
+	/**
+		@see https://api.openfl.org/openfl/display/DisplayObject.html#x
+	**/
 	#if flash
 	public var x:Float;
 	#else
 	public var x(get, set):Float;
 	#end
 
+	/**
+		@see https://api.openfl.org/openfl/display/DisplayObject.html#y
+	**/
 	#if flash
 	public var y:Float;
 	#else
 	public var y(get, set):Float;
 	#end
 
+	/**
+		@see https://api.openfl.org/openfl/display/DisplayObject.html#width
+	**/
 	#if flash
 	public var width:Float;
 	#else
 	public var width(get, set):Float;
 	#end
 
+	/**
+		@see https://api.openfl.org/openfl/display/DisplayObject.html#height
+	**/
 	#if flash
 	public var height:Float;
 	#else
 	public var height(get, set):Float;
 	#end
 
+	/**
+		@see https://api.openfl.org/openfl/display/DisplayObject.html#scaleX
+	**/
 	#if flash
 	public var scaleX:Float;
 	#else
 	public var scaleX(get, set):Float;
 	#end
 
+	/**
+		@see https://api.openfl.org/openfl/display/DisplayObject.html#scaleY
+	**/
 	#if flash
 	public var scaleY:Float;
 	#else
 	public var scaleY(get, set):Float;
 	#end
 
+	/**
+		@see https://api.openfl.org/openfl/display/DisplayObject.html#alpha
+	**/
 	#if flash
 	public var alpha:Float;
 	#else
 	public var alpha(get, set):Float;
 	#end
 
+	/**
+		@see https://api.openfl.org/openfl/display/DisplayObject.html#visible
+	**/
 	#if flash
 	public var visible:Bool;
 	#else
 	public var visible(get, set):Bool;
 	#end
 
+	/**
+		@see https://api.openfl.org/openfl/display/DisplayObject.html#parent
+	**/
 	public var parent(default, never):DisplayObjectContainer;
 
+	/**
+		@see https://api.openfl.org/openfl/display/DisplayObject.html#stage
+	**/
 	public var stage(default, never):Stage;
 }
