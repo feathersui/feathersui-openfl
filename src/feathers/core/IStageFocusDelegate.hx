f00dba34656fa1cb7824f23e3c04541b5eb09831
Josh Tynjala <joshtynjala@bowlerhat.dev>
2022-05-12 14:21:59 -0700

    IStageFocusDelegate: missing license header

--- src/feathers/core/IStageFocusDelegate.hx
+++ src/feathers/core/IStageFocusDelegate.hx
@@ -1,3 +1,11 @@
+/*
+	Feathers UI
+	Copyright 2022 Bowler Hat LLC. All Rights Reserved.
+
+	This program is free software. You can redistribute and/or modify it in
+	accordance with the terms of the accompanying license agreement.
+ */
+
 package feathers.core;
 
 import openfl.display.InteractiveObject;
