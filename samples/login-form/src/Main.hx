0637c40f5ed166ddd6bd058ea61862b3128f04ec
Josh Tynjala <joshtynjala@bowlerhat.dev>
2022-05-19 13:23:51 -0700

    login-form: use errorString, required, and show result in Alert instead of console

--- samples/login-form/src/Main.hx
+++ samples/login-form/src/Main.hx
@@ -1,3 +1,4 @@
+import openfl.events.Event;
 import feathers.controls.Alert;
 import feathers.controls.Application;
 import feathers.controls.Button;
@@ -26,15 +27,19 @@ class Main extends Application {
 		// position the text on any of the four sides
 		emailItem.textPosition = LEFT;
 		emailItem.text = "Email";
+		emailItem.required = true;
 		this.emailInput = new TextInput();
+		this.emailInput.addEventListener(Event.CHANGE, emailInput_changeHandler);
 		emailItem.content = this.emailInput;
 		form.addChild(emailItem);
 
 		var passwordItem = new FormItem();
 		passwordItem.textPosition = LEFT;
 		passwordItem.text = "Password";
+		passwordItem.required = true;
 		this.passwordInput = new TextInput();
 		this.passwordInput.displayAsPassword = true;
+		this.passwordInput.addEventListener(Event.CHANGE, passwordInput_changeHandler);
 		passwordItem.content = this.passwordInput;
 		form.addChild(passwordItem);
 
@@ -52,21 +57,41 @@ class Main extends Application {
 
 	private function resetForm():Void {
 		this.emailInput.text = "";
+		this.emailInput.errorString = null;
 		this.passwordInput.text = "";
+		this.passwordInput.errorString = null;
+	}
+
+	private function emailInput_changeHandler(event:Event):Void {
+		if (this.emailInput.text.length == 0) {
+			return;
+		}
+		this.emailInput.errorString = null;
+	}
+
+	private function passwordInput_changeHandler(event:Event):Void {
+		if (this.passwordInput.text.length == 0) {
+			return;
+		}
+		this.passwordInput.errorString = null;
 	}
 
 	private function form_submitHandler(event:FormEvent):Void {
 		var email = this.emailInput.text;
 		var password = this.passwordInput.text;
+
+		this.emailInput.errorString = null;
+		this.passwordInput.errorString = null;
 		if (email.length == 0) {
-			Alert.show("Email is required.", "Error", ["OK"]);
+			this.emailInput.errorString = "Email is required.";
 			return;
 		}
 		if (password.length == 0) {
-			Alert.show("Password is required.", "Error", ["OK"]);
+			this.passwordInput.errorString = "Password is required.";
 			return;
 		}
-		trace('submit! email: ${email}, password: ${password}');
+
+		Alert.show('Email: ${email}\nPassword: ${password}', "Form Submitted", ["OK"]);
 
 		this.resetForm();
 	}
