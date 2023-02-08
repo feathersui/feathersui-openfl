1dfa261e9c4f41538f4d177d1bc0a3f685ebb337
Josh Tynjala <joshtynjala@bowlerhat.dev>
2023-02-08 09:45:34 -0800

    RestrictedStyleTest: rework these tests and add new test for setting value to default outside of theme

--- test/src/feathers/core/RestrictedStyleTest.hx
+++ test/src/feathers/core/RestrictedStyleTest.hx
@@ -8,24 +8,21 @@
 
 package feathers.core;
 
-import feathers.controls.BasicButton;
-import feathers.controls.ButtonState;
 import openfl.Lib;
-import openfl.display.Sprite;
 import utest.Assert;
 import utest.Test;
 
 @:keep
 @:access(feathers.core.FeathersControl)
 class RestrictedStyleTest extends Test {
-	private var _control:BasicButton;
+	private var _control:CustomStyleControl;
 
 	public function new() {
 		super();
 	}
 
 	public function setup():Void {
-		this._control = new BasicButton();
+		this._control = new CustomStyleControl();
 	}
 
 	public function teardown():Void {
@@ -34,43 +31,78 @@ class RestrictedStyleTest extends Test {
 	}
 
 	public function testStyleNotRestrictedAfterConstructor():Void {
-		Assert.isFalse(this._control.isStyleRestricted("backgroundSkin"), "Style property must not be restricted after constructor.");
+		Assert.isFalse(this._control.isStyleRestricted("style1"), "Style property must not be restricted after constructor.");
+		Assert.isFalse(this._control.isStyleRestricted("style2"), "Style property must not be restricted after constructor.");
 	}
 
 	public function testStyleNotRestrictedAfterInitialize():Void {
 		this._control.initializeNow();
-		Assert.isFalse(this._control.isStyleRestricted("backgroundSkin"), "Style property must not be restricted after initialize.");
+		Assert.isFalse(this._control.isStyleRestricted("style1"), "Style property must not be restricted after initialize.");
+		Assert.isFalse(this._control.isStyleRestricted("style2"), "Style property must not be restricted after initialize.");
 	}
 
 	public function testStyleNotRestrictedAfterValidate():Void {
 		this._control.validateNow();
-		Assert.isFalse(this._control.isStyleRestricted("backgroundSkin"), "Style property must not be restricted after validate.");
+		Assert.isFalse(this._control.isStyleRestricted("style1"), "Style property must not be restricted after validate.");
+		Assert.isFalse(this._control.isStyleRestricted("style2"), "Style property must not be restricted after validate.");
 	}
 
 	public function testRestrictedStyle():Void {
-		this._control.backgroundSkin = new Sprite();
-		Assert.isTrue(this._control.isStyleRestricted("backgroundSkin"), "Setting style property must restrict it.");
-		Assert.isFalse(this._control.isStyleRestricted("textFormat"), "Setting style property must not restrict a different style.");
+		this._control.style1 = "value";
+		Assert.isTrue(this._control.isStyleRestricted("style1"), "Setting style property must restrict it.");
+		Assert.isFalse(this._control.isStyleRestricted("style2"), "Setting style property must not restrict a different style.");
+	}
+
+	public function testRestrictedStyleSetToDefault():Void {
+		this._control.style1 = null;
+		Assert.isTrue(this._control.isStyleRestricted("style1"), "Setting style property must restrict it.");
+		Assert.isFalse(this._control.isStyleRestricted("style2"), "Setting style property must not restrict a different style.");
 	}
 
 	public function testStyleWithStateNotRestrictedAfterConstructor():Void {
-		Assert.isFalse(this._control.isStyleRestricted("setSkinForState", ButtonState.DOWN), "Style with state must not be restricted after constructor.");
+		Assert.isFalse(this._control.isStyleRestricted("setStateStyle", CustomStyleEnum.ONE), "Style with state must not be restricted after constructor.");
+		Assert.isFalse(this._control.isStyleRestricted("setStateStyle", CustomStyleEnum.TWO), "Style with state must not be restricted after constructor.");
 	}
 
 	public function testStyleWithStateNotRestrictedAfterInitialize():Void {
 		this._control.initializeNow();
-		Assert.isFalse(this._control.isStyleRestricted("setSkinForState", ButtonState.DOWN), "Style with state must not be restricted after initialize.");
+		Assert.isFalse(this._control.isStyleRestricted("setStateStyle", CustomStyleEnum.ONE), "Style with state must not be restricted after initialize.");
+		Assert.isFalse(this._control.isStyleRestricted("setStateStyle", CustomStyleEnum.TWO), "Style with state must not be restricted after initialize.");
 	}
 
 	public function testStyleWithStateNotRestrictedAfterValidate():Void {
 		this._control.validateNow();
-		Assert.isFalse(this._control.isStyleRestricted("setSkinForState", ButtonState.DOWN), "Style with state must not be restricted after validate.");
+		Assert.isFalse(this._control.isStyleRestricted("setStateStyle", CustomStyleEnum.ONE), "Style with state must not be restricted after validate.");
+		Assert.isFalse(this._control.isStyleRestricted("setStateStyle", CustomStyleEnum.TWO), "Style with state must not be restricted after validate.");
 	}
 
 	public function testRestrictedStyleWithState():Void {
-		this._control.setSkinForState(ButtonState.DOWN, new Sprite());
-		Assert.isTrue(this._control.isStyleRestricted("setSkinForState", ButtonState.DOWN), "Calling style for state function must restrict it.");
-		Assert.isFalse(this._control.isStyleRestricted("setSkinForState", ButtonState.HOVER),
+		this._control.setStateStyle(CustomStyleEnum.ONE, "value");
+		Assert.isTrue(this._control.isStyleRestricted("setStateStyle", CustomStyleEnum.ONE), "Calling style for state function must restrict it.");
+		Assert.isFalse(this._control.isStyleRestricted("setStateStyle", CustomStyleEnum.TWO),
 			"Calling style for state function must not restrict a different state.");
 	}
+}
+
+private class CustomStyleControl extends FeathersControl {
+	public function new() {
+		super();
+	}
+
+	@:style
+	public var style1:String = null;
+	@:style
+	public var style2:String = null;
+
+	@style
+	public function setStateStyle(state:CustomStyleEnum, value:String):Void {
+		if (!this.setStyle("setStateStyle", state)) {
+			return;
+		}
+	}
+}
+
+private enum CustomStyleEnum {
+	ONE;
+	TWO;
 }
